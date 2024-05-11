132c6ef059c0fdd518cfd12e97c74452e76bb60c
kstechly <kayastechly@gmail.com>
2024-05-11 13:37:41 -0700

    added ssa names and processing

---
+++ data/instances/coinflip/names/README.md
@@ -0,0 +1,1 @@
+Original files are taken from [this](https://www.ssa.gov/oact/babynames/limits.html) Social Security Administration site. Names have been postprocessed to remove additional information (gender, frequency), and only names with over 6000 occurrences have been kept.
---
+++ data/instances/coinflip/names/merge_data.py
@@ -0,0 +1,22 @@
+import os
+import json
+
+
+def read(file_name):
+    with open(file_name, "r") as fp:
+        text = fp.read()
+        return text
+
+file_names = [x for x in os.listdir() if x.startswith('yob')]
+
+names = []
+for file_name in file_names:
+    file_data = read(file_name)
+    file_data = file_data.split("\n")
+    file_data = [x.split(",")[0] for x in file_data if x and int(x.split(",")[2])>6000]
+    names = names + file_data
+
+
+print(len(names))
+with open('ssa_names_data.json',"w") as fp:
+    json.dump(names, fp)
