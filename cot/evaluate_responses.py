c78b1c9a3d388e2d86245fc236fe41b18883a815
kstechly <kayastechly@gmail.com>
2024-06-07 18:09:15 -0700

    messy graph generation stuff

--- cot/evaluate_responses.py
+++ cot/evaluate_responses.py
@@ -38,77 +38,77 @@ def evaluate_responses(domain_name, llm=None, specified_instances=[], overwrite_
     sns.set_context("poster", font_scale=FONTSCALE)
     sns.set_theme(style="darkgrid", font=prop.get_name())
 
-    coinflip = utils.read_json("coinflip", False, "evaluations", verbose=verbose)
-    cf_axis_num = 0
-    flat_cf = utils.flatten(coinflip)
-    cf_df = pd.DataFrame(flat_cf)
-    if llm: cf_df = cf_df.drop(cf_df[cf_df['llm'] != llm].index)
-    cf_df.loc[(cf_df.magic =="Let's think step by step.") & (cf_df.cot==""), 'cot'] = 'Zero-Shot CoT'
-    cf_df.loc[(cf_df.magic ==" ") & (cf_df.cot==""), 'cot'] = 'Thought Tag Only'
-    cf_df.replace({"cot":{"":"direct"}}, inplace=True)
-    cf_subdf = cf_df
-    cf_idict = {"cot":["direct","wei"],"relaxation":["full"]}
-    for k in cf_idict.keys():
-        cf_subdf = cf_subdf[cf_subdf[k].isin(cf_idict[k])]
+    # coinflip = utils.read_json("coinflip", False, "evaluations", verbose=verbose)
+    # cf_axis_num = 0
+    # flat_cf = utils.flatten(coinflip)
+    # cf_df = pd.DataFrame(flat_cf)
+    # if llm: cf_df = cf_df.drop(cf_df[cf_df['llm'] != llm].index)
+    # cf_df.loc[(cf_df.magic =="Let's think step by step.") & (cf_df.cot==""), 'cot'] = 'Zero-Shot CoT'
+    # cf_df.loc[(cf_df.magic ==" ") & (cf_df.cot==""), 'cot'] = 'Thought Tag Only'
+    # cf_df.replace({"cot":{"":"direct"}}, inplace=True)
+    # cf_subdf = cf_df
+    # cf_idict = {"cot":["direct","wei"],"relaxation":["full"]}
+    # for k in cf_idict.keys():
+    #     cf_subdf = cf_subdf[cf_subdf[k].isin(cf_idict[k])]
 
-    cf_subdf['correct'] = cf_subdf['correct'].map(lambda x: 100*x).fillna(cf_df['correct'])
-    # cf_subdf['cot'] = cf_subdf['cot'].map(lambda x: x.capitalize())
-    cf_subdf['cot'] = cf_subdf['cot'].map({"direct":"Direct","wei":"CoT"}).fillna(cf_subdf["cot"])
-    # cf_subdf['relaxation'] = cf_subdf['relaxation'].map({"chain":"Arbitrary", "full":"Arbitrary", "told":"Single-Digit"}).fillna(cf_df['relaxation'])
-    # cf_subdf['relaxation'] = cf_subdf['relaxation'].map(lambda x: x.capitalize())
-    cf_subdf.rename(columns={"cot": "CoT"}, inplace=True)
-    cf_subdf = cf_subdf[cf_subdf.steps_to_solve<30]
-    # subd
-    sns.lineplot(x=x, y=y, style="CoT", data=cf_subdf, palette="deep", err_style=None, style_order=["CoT", "Direct"], ax=axs[cf_axis_num])
-    axs[cf_axis_num].set_title(f'CoinFlip', fontsize=FONTSIZE)
-    axs[cf_axis_num].set_xlabel('# of People', fontsize=FONTSIZE)
-    axs[cf_axis_num].set_ylabel('% of Instances correct', fontsize=FONTSIZE)
-    axs[cf_axis_num].legend(fontsize=LEGENDFONT)
-    axs[cf_axis_num].set_xticks(range(2, 29, 3))
-    axs[cf_axis_num].set_yticks(range(0, 101, 20))
-    axs[cf_axis_num].set_ylim(bottom=0)
-    axs[cf_axis_num].set_xlim(left=1, right=29)
-    axs[cf_axis_num].grid(True)
-    axs[cf_axis_num].tick_params(axis='both', which='major', labelsize=FONTSIZE)
-    axs[cf_axis_num].tick_params(axis='both', which='minor', labelsize=FONTSIZE)
-    # axs[cf_axis_num].plot([cf_subdf.min()[x], cf_subdf.max()[x]], [0.5, 0.5])
-    axs[cf_axis_num].axhline(y=50, linewidth=2, color='orange', ls=':')
+    # cf_subdf['correct'] = cf_subdf['correct'].map(lambda x: 100*x).fillna(cf_df['correct'])
+    # # cf_subdf['cot'] = cf_subdf['cot'].map(lambda x: x.capitalize())
+    # cf_subdf['cot'] = cf_subdf['cot'].map({"direct":"Direct","wei":"CoT"}).fillna(cf_subdf["cot"])
+    # # cf_subdf['relaxation'] = cf_subdf['relaxation'].map({"chain":"Arbitrary", "full":"Arbitrary", "told":"Single-Digit"}).fillna(cf_df['relaxation'])
+    # # cf_subdf['relaxation'] = cf_subdf['relaxation'].map(lambda x: x.capitalize())
+    # cf_subdf.rename(columns={"cot": "CoT"}, inplace=True)
+    # cf_subdf = cf_subdf[cf_subdf.steps_to_solve<30]
+    # # subd
+    # sns.lineplot(x=x, y=y, style="CoT", data=cf_subdf, palette="deep", err_style=None, style_order=["CoT", "Direct"], ax=axs[cf_axis_num])
+    # axs[cf_axis_num].set_title(f'CoinFlip', fontsize=FONTSIZE)
+    # axs[cf_axis_num].set_xlabel('# of People', fontsize=FONTSIZE)
+    # axs[cf_axis_num].set_ylabel('% of Instances correct', fontsize=FONTSIZE)
+    # axs[cf_axis_num].legend(fontsize=LEGENDFONT)
+    # axs[cf_axis_num].set_xticks(range(2, 29, 3))
+    # axs[cf_axis_num].set_yticks(range(0, 101, 20))
+    # axs[cf_axis_num].set_ylim(bottom=0)
+    # axs[cf_axis_num].set_xlim(left=1, right=29)
+    # axs[cf_axis_num].grid(True)
+    # axs[cf_axis_num].tick_params(axis='both', which='major', labelsize=FONTSIZE)
+    # axs[cf_axis_num].tick_params(axis='both', which='minor', labelsize=FONTSIZE)
+    # # axs[cf_axis_num].plot([cf_subdf.min()[x], cf_subdf.max()[x]], [0.5, 0.5])
+    # axs[cf_axis_num].axhline(y=50, linewidth=2, color='orange', ls=':')
 
 
-    ########## LLC
-    llc = utils.read_json("lastletterconcat", False, "evaluations", verbose=verbose)
-    llc_axis_num = 1
-    flat_llc = utils.flatten(llc)
-    llc_df = pd.DataFrame(flat_llc)
-    if llm: llc_df = llc_df.drop(llc_df[llc_df['llm'] != llm].index)
-    # llc_df.loc[(llc_df.magic =="Let's think step by step.") & (llc_df.cot==""), 'cot'] = 'Zero-Shot CoT'
-    # llc_df.loc[(llc_df.magic ==" ") & (llc_df.cot==""), 'cot'] = 'Thought Tag Only'
-    llc_df.replace({"cot":{"":"direct"}}, inplace=True)
-    llc_subdf = llc_df
-    llc_idict = {"cot":["wei","direct"],"relaxation":["full","vowel","foom_clearer"]}
-    for k in llc_idict.keys():
-        llc_subdf = llc_subdf[llc_subdf[k].isin(llc_idict[k])]
+    # ########## LLC
+    # llc = utils.read_json("lastletterconcat", False, "evaluations", verbose=verbose)
+    # llc_axis_num = 1
+    # flat_llc = utils.flatten(llc)
+    # llc_df = pd.DataFrame(flat_llc)
+    # if llm: llc_df = llc_df.drop(llc_df[llc_df['llm'] != llm].index)
+    # # llc_df.loc[(llc_df.magic =="Let's think step by step.") & (llc_df.cot==""), 'cot'] = 'Zero-Shot CoT'
+    # # llc_df.loc[(llc_df.magic ==" ") & (llc_df.cot==""), 'cot'] = 'Thought Tag Only'
+    # llc_df.replace({"cot":{"":"direct"}}, inplace=True)
+    # llc_subdf = llc_df
+    # llc_idict = {"cot":["wei","direct"],"relaxation":["full","vowel","foom_clearer"]}
+    # for k in llc_idict.keys():
+    #     llc_subdf = llc_subdf[llc_subdf[k].isin(llc_idict[k])]
 
-    llc_subdf['correct'] = llc_subdf['correct'].map(lambda x: 100*x).fillna(llc_df['correct'])
-    llc_subdf['cot'] = llc_subdf['cot'].map({"direct":"Direct","wei":"Basic"}).fillna(llc_subdf["cot"])
-    # llc_subdf['cot'] = llc_subdf['cot'].map(lambda x: x.capitalize())
-    # llc_subdf['relaxation'] = llc_subdf['relaxation'].map({"chain":"Arbitrary", "full":"Arbitrary", "told":"Single-Digit"}).fillna(llc_df['relaxation'])
-    # llc_subdf['relaxation'] = llc_subdf['relaxation'].map(lambda x: x.capitalize())
-    llc_subdf.rename(columns={"cot": "CoT", "relaxation": "Variant"}, inplace=True)
-    # subd
-    sns.lineplot(x=x, y=y, hue="Variant", style="CoT", data=llc_subdf, palette="deep", err_style=None, style_order=["Basic", "Direct"], ax=axs[llc_axis_num])
-    axs[llc_axis_num].set_title(f'LastLetterConcatenation', fontsize=FONTSIZE)
-    axs[llc_axis_num].set_xlabel('# of Words', fontsize=FONTSIZE)
-    # axs[llc_axis_num].set_ylabel('% of Instances correct', fontsize=FONTSIZE)
-    axs[llc_axis_num].legend(fontsize=LEGENDFONT)
-    axs[llc_axis_num].set_xticks(range(0, 21, 2))
-    axs[llc_axis_num].set_yticks(range(0, 101, 20))
-    axs[llc_axis_num].set_ylim(bottom=0)
-    axs[llc_axis_num].set_xlim(left=1, right=20)
-    axs[llc_axis_num].grid(True)
-    axs[llc_axis_num].tick_params(axis='both', which='major', labelsize=FONTSIZE)
-    axs[llc_axis_num].tick_params(axis='both', which='minor', labelsize=FONTSIZE)
-    plt.plot([llc_subdf.min()[x], llc_subdf.max()[x]], [0.5, 0.5])
+    # llc_subdf['correct'] = llc_subdf['correct'].map(lambda x: 100*x).fillna(llc_df['correct'])
+    # llc_subdf['cot'] = llc_subdf['cot'].map({"direct":"Direct","wei":"Basic"}).fillna(llc_subdf["cot"])
+    # # llc_subdf['cot'] = llc_subdf['cot'].map(lambda x: x.capitalize())
+    # # llc_subdf['relaxation'] = llc_subdf['relaxation'].map({"chain":"Arbitrary", "full":"Arbitrary", "told":"Single-Digit"}).fillna(llc_df['relaxation'])
+    # # llc_subdf['relaxation'] = llc_subdf['relaxation'].map(lambda x: x.capitalize())
+    # llc_subdf.rename(columns={"cot": "CoT", "relaxation": "Variant"}, inplace=True)
+    # # subd
+    # sns.lineplot(x=x, y=y, hue="Variant", style="CoT", data=llc_subdf, palette="deep", err_style=None, style_order=["Basic", "Direct"], ax=axs[llc_axis_num])
+    # axs[llc_axis_num].set_title(f'LastLetterConcatenation', fontsize=FONTSIZE)
+    # axs[llc_axis_num].set_xlabel('# of Words', fontsize=FONTSIZE)
+    # # axs[llc_axis_num].set_ylabel('% of Instances correct', fontsize=FONTSIZE)
+    # axs[llc_axis_num].legend(fontsize=LEGENDFONT)
+    # axs[llc_axis_num].set_xticks(range(0, 21, 2))
+    # axs[llc_axis_num].set_yticks(range(0, 101, 20))
+    # axs[llc_axis_num].set_ylim(bottom=0)
+    # axs[llc_axis_num].set_xlim(left=1, right=20)
+    # axs[llc_axis_num].grid(True)
+    # axs[llc_axis_num].tick_params(axis='both', which='major', labelsize=FONTSIZE)
+    # axs[llc_axis_num].tick_params(axis='both', which='minor', labelsize=FONTSIZE)
+    # plt.plot([llc_subdf.min()[x], llc_subdf.max()[x]], [0.5, 0.5])
 
 
 
@@ -137,13 +137,13 @@ def evaluate_responses(domain_name, llm=None, specified_instances=[], overwrite_
     df.loc[(df.magic ==" ") & (df.cot==""), 'cot'] = 'Thought Tag Only'
     df.replace({"cot":{"":"direct"}}, inplace=True)
     boolean_table = df.select_dtypes(np.bool_).value_counts(normalize=True).mul(100).astype(str)
-    print(boolean_table+"%")
+    # print(boolean_table+"%")
     # print(df.pivot_table(columns='uniform_token_length', values='correct'))
     # df['binned'] = pd.cut(df['input_length'],5)
     # print(df.pivot_table(index='steps_to_solve',columns='binned', values='correct'))
-    print(f"\n${df['estimated_cost'].sum():.02f} estimated total spend.\n")
+    # print(f"\n${df['estimated_cost'].sum():.02f} estimated total spend.\n")
     df2 = df.drop(columns=['trial_id', 'temp', 'n_examples', 'output_length', 'well_formed_response', 'timestamp', 'estimated_cost'])
-    print(df2.corr(numeric_only=True))
+    # print(df2.corr(numeric_only=True))
     # steps_pivot = df.pivot_table(columns='steps_to_solve', values='correct')
     # print(steps_pivot.head())
     # print(steps_pivot.columns)
@@ -152,6 +152,8 @@ def evaluate_responses(domain_name, llm=None, specified_instances=[], overwrite_
     print(idict)
     for k in idict.keys():
         subdf = subdf[subdf[k].isin(idict[k])]
+    if domain_name == "coinflip":
+        subdf = subdf[subdf.steps_to_solve<30]
     # subdf = subdf[subdf.steps_to_solve>1]
     if values and columns:
         print("\n")
