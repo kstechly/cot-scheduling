3558ab4cae144c15ec86d4cca88b10f2ff892252
kstechly <kayastechly@gmail.com>
2024-06-07 18:10:01 -0700

    added digitester domain

--- cot/domain_utils/__init__.py
+++ cot/domain_utils/__init__.py
@@ -1,6 +1,6 @@
 # To add a new domain, create a module in this subfolder, and then add it to the following:
-from domain_utils import color_verification, coinflip, lastletterconcat, sorting, pemdas, fib, modarith
-domains = {"color_verification":color_verification, "coinflip":coinflip, "lastletterconcat":lastletterconcat, "sorting":sorting, "pemdas":pemdas, "fib":fib, "modarith": modarith}
+from domain_utils import color_verification, coinflip, lastletterconcat, sorting, pemdas, fib, modarith, digitester
+domains = {"color_verification":color_verification, "coinflip":coinflip, "lastletterconcat":lastletterconcat, "sorting":sorting, "pemdas":pemdas, "fib":fib, "modarith": modarith, "digitester": digitester}
 __all__ = list(domains.keys())
 
 # TODO refactor this to autopopulate
