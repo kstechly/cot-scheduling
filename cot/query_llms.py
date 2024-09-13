ee12c41b847ebfbf31263776aa423144f263cd88
kstechly <kstechly@gmail.com>
2024-09-13 10:32:46 -0700

    getting multithreading running, still issues

--- cot/query_llms.py
+++ cot/query_llms.py
@@ -7,6 +7,8 @@ from groq import Groq #type: ignore
 import os
 import requests
 import boto3 #type: ignore
+import concurrent.futures
+import time
 
 import utils
 
@@ -23,7 +25,8 @@ input_costs_per_million  = {
     "gpt-3.5-turbo-0125": 0.5, 
     "llama3-8b-8192": 0.0, 
     "gpt-4o-2024-05-13": 5, 
-    "gpt-4o-mini-2024-07-18":0.15}
+    "gpt-4o-mini-2024-07-18":0.15,
+    "o1-preview":15}
 output_costs_per_million = {
     "gpt-4": 60, 
     "gpt-4-turbo":30, 
@@ -31,7 +34,8 @@ output_costs_per_million = {
     "gpt-3.5-turbo-0125": 1.5, 
     "llama3-8b-8192": 0.0, 
     "gpt-4o-2024-05-13": 15, 
-    "gpt-4o-mini-2024-07-18": 0.6}
+    "gpt-4o-mini-2024-07-18": 0.6,
+    "o1-preview":60}
 
 def get_responses(llm, domain_name, specified_instances = [], print_models=False, overwrite_previous=False, verbose=False, temp=0, num_trials=1, **prompt_specification):
     if print_models:
@@ -72,7 +76,7 @@ def get_responses(llm, domain_name, specified_instances = [], print_models=False
 
     # Load prompts and possibly filter for specified_instances
     instances = utils.read_json(domain_name, False, "prompts", verbose)
-    if specified_instances: working_instances = {num: instances[num] for num in instances.key() if num in specified_instances}
+    if specified_instances: working_instances = {num: instances[num] for num in instances.keys() if num in specified_instances}
     else: working_instances = instances
 
     # Load previously done work
@@ -83,66 +87,74 @@ def get_responses(llm, domain_name, specified_instances = [], print_models=False
 
     failed_instances = []
     write_counter = 0
-    with prog as p:
-        for instance in p.track(working_instances, description=f"{llm}, {domain_name}"):
-            instance_task = utils.replace_task(p, instance_task, description=f'Instance {instance} (Cost so far: ${total_cost:.02f})', total=len(working_instances[instance]))
-            if instance not in previous.keys(): previous[instance] = []
-            previous_instance_output = previous[instance]         
-            for prompt in working_instances[instance]:
-                if not utils.includes_dict([prompt], prompt_specification):
-                    if "examples_all" in prompt_specification.values():
-                        if "examples_" not in prompt["example_type"]:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=5) as executor:
+        with prog as p:
+            def process_item(instance):
+                print(f'processing {instance}')
+                #instance_task = utils.replace_task(p, instance_task, description=f'Instance {instance} (Cost so far: ${total_cost:.02f})', total=len(working_instances[instance]))
+                if instance not in previous.keys(): previous[instance] = []
+                previous_instance_output = previous[instance]         
+                for prompt in working_instances[instance]:
+                    print(f'processing prompt {prompt} of instance {instance}')
+                    if not utils.includes_dict([prompt], prompt_specification):
+                        if "examples_all" in prompt_specification.values():
+                            if "examples_" not in prompt["example_type"]:
+                                p.update(instance_task, advance=1)
+                                continue
+                        else: 
                             p.update(instance_task, advance=1)
+                            print(f'rejecting prompt {prompt} of {instance} bc {prompt_specification}')
                             continue
-                    else: 
-                        p.update(instance_task, advance=1)
-                        continue
-                for trial_id in range(0, num_trials):
-                    trial_specification = {"trial_id": trial_id, "llm": llm, "temp": temp}
-                    trial_specification.update(prompt)
-                    if utils.includes_dict(previous_instance_output, trial_specification): continue
-                    trial_specification.pop('prompt',None)
+                    for trial_id in range(0, num_trials):
+                        print(trial_id)
+                        trial_specification = {"trial_id": trial_id, "llm": llm, "temp": temp}
+                        trial_specification.update(prompt)
+                        if utils.includes_dict(previous_instance_output, trial_specification): continue
+                        trial_specification.pop('prompt',None)
 
-                    if utils.includes_dict(previous_instance_output, trial_specification) and not overwrite_previous: continue
-                    ind = utils.dict_index(previous_instance_output, trial_specification)
+                        if utils.includes_dict(previous_instance_output, trial_specification) and not overwrite_previous: continue
+                        ind = utils.dict_index(previous_instance_output, trial_specification)
 
-                    prompt_text = prompt["prompt"]
-                    if llm != "gpt-4o-mini-2024-07-18": # TODO get rid of all the lines that start with this. It's just a hack to speed things up right now!!! Also go back and calc costs for the ones I'm skipping
-                        token_length = len(enc.encode(prompt_text))
-                        trial_cost = token_length*input_cost
-                    if verbose:
-                        if llm != "gpt-4o-mini-2024-07-18": print(f"==Instance: {instance}, Tokens: {token_length}==")
-                        else: print(f"==Instance: {instance}")
-                        info_dict = {x: trial_specification[x] for x in trial_specification.keys()}
-                        print(f'=={info_dict}==')
-                        print(prompt_text)
-                    trial_output = trial_specification
-                    trial_output.update(prompt)
+                        prompt_text = prompt["prompt"]
+                        if llm != "gpt-4o-mini-2024-07-18": # TODO get rid of all the lines that start with this. It's just a hack to speed things up right now!!! Also go back and calc costs for the ones I'm skipping
+                            token_length = len(enc.encode(prompt_text))
+                            trial_cost = token_length*input_cost
+                        if verbose:
+                            if llm != "gpt-4o-mini-2024-07-18": print(f"==Instance: {instance}, Tokens: {token_length}==")
+                            else: print(f"==Instance: {instance}")
+                            info_dict = {x: trial_specification[x] for x in trial_specification.keys()}
+                            print(f'=={info_dict}==')
+                            print(prompt_text)
+                        trial_output = trial_specification
+                        trial_output.update(prompt)
 
-                    llm_response = send_query(prompt_text, llm, temp=temp)
-                    if not llm_response:
-                        failed_instances.append(instance)
-                        print(f"==Failed instance: {instance}==")
-                        continue
-                    if llm != "gpt-4o-mini-2024-07-18":
-                        trial_cost += len(enc.encode(llm_response))*output_cost
-                        current_sesh_cost += trial_cost
-                        total_cost += trial_cost
+                        llm_response = send_query(prompt_text, llm, temp=temp)
+                        if not llm_response:
+                            failed_instances.append(instance)
+                            print(f"==Failed instance: {instance}==")
+                            continue
+                        if llm != "gpt-4o-mini-2024-07-18":
+                            trial_cost += len(enc.encode(llm_response))*output_cost
+                            current_sesh_cost += trial_cost
+                            total_cost += trial_cost
 
-                    if llm != "gpt-4o-mini-2024-07-18": trial_output.update({"response": llm_response, "timestamp": time.time(), "estimated_cost": trial_cost})
-                    else: trial_output.update({"response": llm_response, "timestamp": time.time()})
-                    # print(f'Trial output: {trial_output}')
-                    if ind == -1: previous[instance].append(trial_output)
-                    else: previous[instance][ind] = trial_output
-                    if write_counter >= WRITING_DELAY:
-                        utils.write_json(domain_name, previous, "responses")
-                        write_counter = 0
-                    else: write_counter+=1
-                    if verbose:     
-                        print(f'==LLM Response==')
-                        print(llm_response)
-                        # print(f"***Current cost: {current_sesh_cost:.4f}***")
-                p.update(instance_task, advance=1)
+                        if llm != "gpt-4o-mini-2024-07-18": trial_output.update({"response": llm_response, "timestamp": time.time(), "estimated_cost": trial_cost})
+                        else: trial_output.update({"response": llm_response, "timestamp": time.time()})
+                        # print(f'Trial output: {trial_output}')
+                        if ind == -1: previous[instance].append(trial_output)
+                        else: previous[instance][ind] = trial_output
+                        if write_counter >= WRITING_DELAY:
+                            utils.write_json(domain_name, previous, "responses")
+                            write_counter = 0
+                        else: write_counter+=1
+                        if verbose:     
+                            print(f'==LLM Response==')
+                            print(llm_response)
+                            # print(f"***Current cost: {current_sesh_cost:.4f}***")
+                    #p.update(instance_task, advance=1)
+            print('e time')
+            executor.map(process_item, working_instances)
+            print('e over')
 
     # a final write, if using writing_delay
     utils.write_json(domain_name, previous, "responses")                    
@@ -177,15 +189,16 @@ def is_groq_model(llm):
 
 def send_query(query_text, llm, temp=0, stop_statement=STOP_STATEMENT):
     messages=[
-        {"role": "system", "content": SYSTEM_PROMPT},
+        #{"role": "system", "content": SYSTEM_PROMPT},
         {"role": "user", "content": query_text}
         ]
     if is_openai_model(llm):
         try:
             client = openai.OpenAI()
-            response = client.chat.completions.create(model=llm, messages=messages, temperature=temp, stop=stop_statement)
+            response = client.chat.completions.create(model=llm, messages=messages)#, temperature=temp, stop=stop_statement)
         except Exception as e:
             print("[-]: Failed GPT query execution: {}".format(e))
+            time.sleep(3000)
             return ""
         text_response = response.choices[0].message.content
         return text_response.strip()
