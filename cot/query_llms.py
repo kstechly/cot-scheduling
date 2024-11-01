395ac13e321d9e10e29609a2802b571457b7cfbd
kstechly <kayastechly@gmail.com>
2024-10-31 22:55:20 -0700

    pep8 issue fixes

--- cot/query_llms.py
+++ cot/query_llms.py
@@ -1,41 +1,45 @@
-import openai # type: ignore
-import tiktoken # type: ignore
-from fire import Fire # type: ignore
+import openai  # type: ignore
+import tiktoken  # type: ignore
+from fire import Fire  # type: ignore
 from functools import cache
 import time
-from groq import Groq #type: ignore
+from groq import Groq  # type: ignore
 import os
 import requests
-import boto3 #type: ignore
+import boto3  # type: ignore
 
-import utils
+from cot import utils
 
-STOP_STATEMENT = "[ANSWER END]" # what we look for to end LLM response generation
+# what we look for to end LLM response generation
+STOP_STATEMENT = "[ANSWER END]"
 
-SYSTEM_PROMPT  = "You are a system that solves reasoning problems presented in text form."
+SYSTEM_PROMPT = "You are a system that solves reasoning problems presented in text form."
 
-WRITING_DELAY  = 10
+WRITING_DELAY = 10
 
-input_costs_per_million  = {
-    "gpt-4": 30, 
-    "gpt-4-turbo":10, 
-    "gpt-4-turbo-2024-04-09": 10, 
-    "gpt-3.5-turbo-0125": 0.5, 
-    "llama3-8b-8192": 0.0, 
-    "gpt-4o-2024-05-13": 5, 
-    "gpt-4o-mini-2024-07-18":0.15}
+input_costs_per_million = {
+    "gpt-4": 30,
+    "gpt-4-turbo": 10,
+    "gpt-4-turbo-2024-04-09": 10,
+    "gpt-3.5-turbo-0125": 0.5,
+    "llama3-8b-8192": 0.0,
+    "gpt-4o-2024-05-13": 5,
+    "gpt-4o-mini-2024-07-18": 0.15}
 output_costs_per_million = {
-    "gpt-4": 60, 
-    "gpt-4-turbo":30, 
-    "gpt-4-turbo-2024-04-09": 30, 
-    "gpt-3.5-turbo-0125": 1.5, 
-    "llama3-8b-8192": 0.0, 
-    "gpt-4o-2024-05-13": 15, 
+    "gpt-4": 60,
+    "gpt-4-turbo": 30,
+    "gpt-4-turbo-2024-04-09": 30,
+    "gpt-3.5-turbo-0125": 1.5,
+    "llama3-8b-8192": 0.0,
+    "gpt-4o-2024-05-13": 15,
     "gpt-4o-mini-2024-07-18": 0.6}
 
-def get_responses(llm, domain_name, specified_instances = [], print_models=False, overwrite_previous=False, verbose=False, temp=0, num_trials=1, **prompt_specification):
+
+def get_responses(llm, domain_name, specified_instances=[],
+                  print_models=False, overwrite_previous=False, verbose=False,
+                  temp=0, num_trials=1, **prompt_specification):
     if print_models:
-        #TODO refactor this
+        # TODO refactor this
         client = openai.OpenAI()
         openai_models = client.models.list().data
         print([x.id for x in openai_models])
@@ -56,27 +60,39 @@ def get_responses(llm, domain_name, specified_instances = [], print_models=False
         print("This isn't the right model, is it??? " + llm)
         # exit()
     # Cost calculation setup
-    input_cost  = 0.0
+    input_cost = 0.0
     output_cost = 0.0
     million = 1000000
     if llm in input_costs_per_million.keys():
-        input_cost  = input_costs_per_million[llm]/million
+        input_cost = input_costs_per_million[llm]/million
         output_cost = output_costs_per_million[llm]/million
-    else: raise ValueError(f"Can't calculate cost for llm {llm}. Change query_llms to fix this.")
+    else:
+        raise ValueError(
+            f"Can't calculate cost for llm {llm}. Change query_llms to fix this.")
     current_sesh_cost = 0.0
-    enc  = tiktoken.get_encoding("cl100k_base")
+    enc = tiktoken.get_encoding("cl100k_base")
 
-    #TODO janky, plz fix
-    try: total_cost = utils.get_total_cost(domain_name)
-    except: total_cost = 0
+    # TODO janky, plz fix
+    try:
+        total_cost = utils.get_total_cost(domain_name)
+    except BaseException:
+        total_cost = 0
 
     # Load prompts and possibly filter for specified_instances
     instances = utils.read_json(domain_name, False, "prompts", verbose)
-    if specified_instances: working_instances = {num: instances[num] for num in instances.key() if num in specified_instances}
-    else: working_instances = instances
+    if specified_instances:
+        working_instances = {
+            num: instances[num] for num in instances.key()
+            if num in specified_instances}
+    else:
+        working_instances = instances
 
     # Load previously done work
-    previous = utils.read_json(domain_name, overwrite_previous, "responses", verbose=verbose)
+    previous = utils.read_json(
+        domain_name,
+        overwrite_previous,
+        "responses",
+        verbose=verbose)
 
     prog = utils.progress_bar()
     instance_task = None
@@ -84,10 +100,15 @@ def get_responses(llm, domain_name, specified_instances = [], print_models=False
     failed_instances = []
     write_counter = 0
     with prog as p:
-        for instance in p.track(working_instances, description=f"{llm}, {domain_name}"):
-            instance_task = utils.replace_task(p, instance_task, description=f'Instance {instance} (Cost so far: ${total_cost:.02f})', total=len(working_instances[instance]))
-            if instance not in previous.keys(): previous[instance] = []
-            previous_instance_output = previous[instance]         
+        for instance in p.track(
+                working_instances, description=f"{llm}, {domain_name}"):
+            instance_task = utils.replace_task(
+                p, instance_task,
+                description=f'Instance {instance} (Cost so far: ${total_cost:.02f})',
+                total=len(working_instances[instance]))
+            if instance not in previous.keys():
+                previous[instance] = []
+            previous_instance_output = previous[instance]
             for prompt in working_instances[instance]:
                 if not utils.includes_dict([prompt], prompt_specification):
                     if "examples_all" in prompt_specification.values():
@@ -95,22 +116,33 @@ def get_responses(llm, domain_name, specified_instances = [], print_models=False
                             p.update(instance_task, advance=1)
                             continue
                 for trial_id in range(0, num_trials):
-                    trial_specification = {"trial_id": trial_id, "llm": llm, "temp": temp}
+                    trial_specification = {
+                        "trial_id": trial_id, "llm": llm, "temp": temp}
                     trial_specification.update(prompt)
-                    if utils.includes_dict(previous_instance_output, trial_specification): continue
-                    trial_specification.pop('prompt',None)
+                    if utils.includes_dict(
+                            previous_instance_output, trial_specification):
+                        continue
+                    trial_specification.pop('prompt', None)
 
-                    if utils.includes_dict(previous_instance_output, trial_specification) and not overwrite_previous: continue
-                    ind = utils.dict_index(previous_instance_output, trial_specification)
+                    if utils.includes_dict(
+                            previous_instance_output, trial_specification) and not overwrite_previous:
+                        continue
+                    ind = utils.dict_index(
+                        previous_instance_output, trial_specification)
 
                     prompt_text = prompt["prompt"]
-                    if llm != "gpt-4o-mini-2024-07-18": # TODO get rid of all the lines that start with this. It's just a hack to speed things up right now!!! Also go back and calc costs for the ones I'm skipping
+                    if llm != "gpt-4o-mini-2024-07-18":  # TODO get rid of all the lines that start with this. It's just a hack to speed things up right now!!! Also go back and calc costs for the ones I'm skipping
                         token_length = len(enc.encode(prompt_text))
                         trial_cost = token_length*input_cost
                     if verbose:
-                        if llm != "gpt-4o-mini-2024-07-18": print(f"==Instance: {instance}, Tokens: {token_length}==")
-                        else: print(f"==Instance: {instance}")
-                        info_dict = {x: trial_specification[x] for x in trial_specification.keys()}
+                        if llm != "gpt-4o-mini-2024-07-18":
+                            print(
+                                f"==Instance: {instance}, Tokens: {token_length}==")
+                        else:
+                            print(f"==Instance: {instance}")
+                        info_dict = {
+                            x: trial_specification[x]
+                            for x in trial_specification.keys()}
                         print(f'=={info_dict}==')
                         print(prompt_text)
                     trial_output = trial_specification
@@ -126,39 +158,53 @@ def get_responses(llm, domain_name, specified_instances = [], print_models=False
                         current_sesh_cost += trial_cost
                         total_cost += trial_cost
 
-                    if llm != "gpt-4o-mini-2024-07-18": trial_output.update({"response": llm_response, "timestamp": time.time(), "estimated_cost": trial_cost})
-                    else: trial_output.update({"response": llm_response, "timestamp": time.time()})
+                    if llm != "gpt-4o-mini-2024-07-18":
+                        trial_output.update(
+                            {"response": llm_response,
+                             "timestamp": time.time(),
+                             "estimated_cost": trial_cost})
+                    else:
+                        trial_output.update(
+                            {"response": llm_response,
+                             "timestamp": time.time()})
                     # print(f'Trial output: {trial_output}')
-                    if ind == -1: previous[instance].append(trial_output)
-                    else: previous[instance][ind] = trial_output
+                    if ind == -1:
+                        previous[instance].append(trial_output)
+                    else:
+                        previous[instance][ind] = trial_output
                     if write_counter >= WRITING_DELAY:
                         utils.write_json(domain_name, previous, "responses")
                         write_counter = 0
-                    else: write_counter+=1
-                    if verbose:     
+                    else:
+                        write_counter += 1
+                    if verbose:
                         print(f'==LLM Response==')
                         print(llm_response)
                         # print(f"***Current cost: {current_sesh_cost:.4f}***")
                 p.update(instance_task, advance=1)
 
     # a final write, if using writing_delay
-    utils.write_json(domain_name, previous, "responses")                    
+    utils.write_json(domain_name, previous, "responses")
     # Print any failed instances
     print(f"Failed instances: {failed_instances}")
     print(f"Total Session Cost: {current_sesh_cost:.2f}")
     print(f"Total Cost: {total_cost:.2f}")
-    
+
 
 @cache
 def is_openai_model(llm):
     client = openai.OpenAI()
     openai_models = client.models.list().data
     return llm in [x.id for x in openai_models]
+
+
 @cache
 def is_bedrock_model(llm):
 
-    #TODO
+    # TODO
     pass
+
+
 @cache
 def is_groq_model(llm):
     api_key = os.environ.get("GROQ_API_KEY")
@@ -172,25 +218,28 @@ def is_groq_model(llm):
     groq_models = [x['id'] for x in d]
     return llm in groq_models
 
+
 def send_query(query_text, llm, temp=0, stop_statement=STOP_STATEMENT):
-    messages=[
+    messages = [
         {"role": "system", "content": SYSTEM_PROMPT},
         {"role": "user", "content": query_text}
         ]
     if is_openai_model(llm):
         try:
             client = openai.OpenAI()
-            response = client.chat.completions.create(model=llm, messages=messages, temperature=temp, stop=stop_statement)
+            response = client.chat.completions.create(
+                model=llm, messages=messages, temperature=temp, stop=stop_statement)
         except Exception as e:
             print("[-]: Failed GPT query execution: {}".format(e))
             return ""
         text_response = response.choices[0].message.content
         return text_response.strip()
     elif is_groq_model(llm):
-        #TODO add some way to slow it down if it goes too fast!
-        try: 
+        # TODO add some way to slow it down if it goes too fast!
+        try:
             client = Groq(api_key=os.environ.get("GROQ_API_KEY"))
-            response = client.chat.completions.create(model=llm, messages=messages, temperature=temp, stop=stop_statement)
+            response = client.chat.completions.create(
+                model=llm, messages=messages, temperature=temp, stop=stop_statement)
         except Exception as e:
             print("[-]: Failed Groq query execution: {}".format(e))
             return ""
@@ -198,7 +247,10 @@ def send_query(query_text, llm, temp=0, stop_statement=STOP_STATEMENT):
         return text_response.strip()
     elif is_bedrock_model(llm):
         raise NotImplementedError("Haven't implemented bedrock prompting yet!")
-    else: raise NotImplementedError(f"Evaluating on \"{llm}\" is not implemented yet!")
+    else:
+        raise NotImplementedError(
+            f"Evaluating on \"{llm}\" is not implemented yet!")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     Fire(get_responses)
